8041b473e5b91d34532576fd3f8a133b7837cfd4
kristinatam <kristina.tamulyte@gmail.com>
2016-10-11 20:28:39 +0300

    Module pattern calculator
    Module pattern calculator-plus,minus,divide,multiply

---
+++ module.js
@@ -0,0 +1,34 @@
+var calculate = (function () { 
+    var i = 0;
+        
+    var plus = function (j) {
+        i +=j;
+        return this;
+    }
+    var minus = function (j){
+        i -=j;
+        return this;
+    }
+    var multiply = function (j) {  
+        i *=j;
+        return this;
+    }
+    var divide = function (j) {
+        i /=j;
+        return this;
+    }
+    var print = function () {
+        console.log(i);    
+    }
+
+    return {
+    plus: plus,
+    minus: minus,
+    multiply: multiply,
+    divide: divide,
+    print: print
+    }
+  
+})();
+
+calculate.plus(20).minus(4).multiply(2).divide(4).print();
